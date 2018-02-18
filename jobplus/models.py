43049ecdc0efedf50c228f003dcd43e1e97f7e8a
tangooow <32192028+tangooow@users.noreply.github.com>
2018-02-18 16:01:45 +0800

    fix #11 搭建基本框架

--- jobplus/models.py
+++ jobplus/models.py
@@ -2,12 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Base(db.Model):
-    __abstract__ = True
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime,
-                           default=datetime.utcnow,
-                           onupdate=datetime.utcnow)
+
 
 
 class User(db.Model):
